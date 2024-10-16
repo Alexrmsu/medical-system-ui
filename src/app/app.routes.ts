23d6dd8d30bb2ef89d3e8f476619edbd99a68f6c
Alexrmsu <alexmagnasura@gmail.com>
2024-10-15 23:45:42 -0300

    routes added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,17 @@
 import { Routes } from '@angular/router';
 import {NotFoundComponent} from "./shared/components/not-found.component";
+import {DashboardComponent} from "./features/dashboard/dashboard.component";
 
 export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'dashboard',
+    pathMatch: 'full'
+  },
+  {
+    path: 'dashboard',
+    loadChildren: () => import('./features/dashboard/dashboard.component').then(m => m.DashboardComponent)
+  },
   {
     path: '**',
     component: NotFoundComponent
