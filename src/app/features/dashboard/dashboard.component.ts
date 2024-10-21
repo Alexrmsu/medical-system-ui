ba0b1c027fb7ebe0f819e8c208c3ea131493ab02
Alexrmsu <alexmagnasura@gmail.com>
2024-10-21 01:03:57 -0300

    dashboard initial code

--- src/app/features/dashboard/dashboard.component.ts
+++ src/app/features/dashboard/dashboard.component.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
+import {MatIcon} from "@angular/material/icon";
+import {MatIconButton} from "@angular/material/button";
+import {MatToolbar} from "@angular/material/toolbar";
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [
+    MatIcon,
+    MatIconButton,
+    MatToolbar
+  ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
